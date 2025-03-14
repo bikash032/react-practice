bf771444941586489befadb11d1791868c0d23b0
bikash032 <bishokarma1996@gmail.com>
2025-03-14 21:26:54 +0545

    tailwind error ressolve

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import "./assets/css/main.css"
 import { createRoot } from "react-dom/client";
-// import LoginPage from "./pages/login/login.page";
-import LoginPage1 from "./pages/login/loginClass.pages";
+
 import { StrictMode } from "react";
-import Example from "./pages/login/login.page";
+
 
 
 
@@ -12,9 +11,6 @@ createRoot(document.getElementById("root")!).render(
 
 <StrictMode>
 <>
-<Example></Example>
-<LoginPage1/>
-
   </>
 </StrictMode>
 );
